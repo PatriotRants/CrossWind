966b1ac892b810207fc43139d6ac5e9b6f9375e2
David Boarman <157320821+PatriotRants@users.noreply.github.com>
2024-02-19 11:58:16 -0600

    imgui demo
    imgui demo is done at the most basic level...needs streamlining.

--- src/Lib/CrossWind/Components/Window.cs
+++ src/Lib/CrossWind/Components/Window.cs
@@ -1,8 +1,5 @@
 using OpenTK.Mathematics;
-using OpenTK.Graphics.OpenGL4;
-using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
-using OpenTK.Windowing.GraphicsLibraryFramework;
 
 using ForgeWorks.CrossWind.Presentation;
 
@@ -13,7 +10,6 @@ internal class Window : GameWindow
     internal bool IsDirty { get; set; }
     internal Color4 Background { get; set; } = Color.LightGray;
 
-    internal event Action OnWindowLoaded;
     internal WindowController Controller { get; init; }
 
     internal Window() : this((800, 600), nameof(Window), "Window") { }
@@ -25,37 +21,6 @@ internal class Window : GameWindow
     { }
     private Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
     {
-        IsDirty = true;
-    }
-
-    protected override void OnLoad()
-    {
-        OnWindowLoaded?.Invoke();
-    }
-    protected override void OnUpdateFrame(FrameEventArgs args)
-    {
-        base.OnUpdateFrame(args);
-    }
-    protected override void OnRenderFrame(FrameEventArgs args)
-    {
-        base.OnRenderFrame(args);
-    }
-    protected override void OnResize(ResizeEventArgs e)
-    {
-        base.OnResize(e);
-
-        IsDirty = true;
-    }
-    protected override void OnMaximized(MaximizedEventArgs e)
-    {
-        base.OnMaximized(e);
-
-        IsDirty = true;
-    }
-    protected override void OnMinimized(MinimizedEventArgs e)
-    {
-        base.OnMinimized(e);
-
         IsDirty = true;
     }
 }
